634534e28769b749d9e426e0a643232805b02e88
Ricardo Miralles <ricmiber@gmail.com>
2023-01-11 14:00:33 +0100

    Coding Challenge #02

---
+++ reverseWords.js
@@ -0,0 +1,9 @@
+// CHALLENGE
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// SOLUTION
+function reverseWords(str){
+    return str.split(" ").reverse().toString().replaceAll(',',' ')
+  }
